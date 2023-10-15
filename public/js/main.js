b9e2ac47756a6593e9ce0430142fb3515d61e822
Jonas <p429344@gmail.com>
2023-10-15 16:31:12 +0200

    update new website

--- public/js/main.js
+++ public/js/main.js
@@ -1,104 +1,115 @@
-(function () {
-  /*=====================================
-    Sticky
-    ======================================= */
-  window.onscroll = function () {
-    const header_navbar = document.querySelector(".navbar-area");
-    const sticky = header_navbar.offsetTop;
-    const logo = document.querySelector(".navbar-brand div");
+(function ($) {
+    "use strict";
 
-    if (window.pageYOffset > sticky) {
-      header_navbar.classList.add("sticky");
-      logo.style.display = "block";
-    } else {
-      header_navbar.classList.remove("sticky");
-      logo.src = "/img/logo/logo_transparent.webp";
-      logo.style.display = "none";
-    }
+    // Spinner
+    var spinner = function () {
+        setTimeout(function () {
+            if ($('#spinner').length > 0) {
+                $('#spinner').removeClass('show');
+            }
+        }, 1);
+    };
+    spinner();
+    
+    
+    // Initiate the wowjs
+    new WOW().init();
 
-    // show or hide the back-top-top button
-    const backToTo = document.querySelector(".scroll-top");
-    if (
-      document.body.scrollTop > 50 ||
-      document.documentElement.scrollTop > 50
-    ) {
-      backToTo.style.display = "flex";
-    } else {
-      backToTo.style.display = "none";
-    }
-  };
 
-  // for menu scroll
-  const pageLink = document.querySelectorAll(".page-scroll");
-
-  pageLink.forEach((elem) => {
-    elem.addEventListener("click", (e) => {
-      e.preventDefault();
-      document.querySelector(elem.getAttribute("href")).scrollIntoView({
-        behavior: "smooth",
-        offsetTop: 1 - 60,
-      });
+    // Fixed Navbar
+    $(window).scroll(function () {
+        if ($(window).width() < 992) {
+            if ($(this).scrollTop() > 45) {
+                $('.fixed-top').addClass('bg-white shadow');
+            } else {
+                $('.fixed-top').removeClass('bg-white shadow');
+            }
+        } else {
+            if ($(this).scrollTop() > 45) {
+                $('.fixed-top').addClass('bg-dark shadow').css('top', -45);
+            } else {
+                $('.fixed-top').removeClass('bg-dark shadow').css('top', 0);
+            }
+        }
     });
-  });
+    
+    
+    // Back to top button
+    $(window).scroll(function () {
+        if ($(this).scrollTop() > 300) {
+            $('.back-to-top').fadeIn('slow');
+        } else {
+            $('.back-to-top').fadeOut('slow');
+        }
+    });
+    $('.back-to-top').click(function () {
+        $('html, body').animate({scrollTop: 0}, 1500, 'easeInOutExpo');
+        return false;
+    });
 
-  // section menu active
-  function onScroll(event) {
-    const sections = document.querySelectorAll(".page-scroll");
-    const scrollPos =
-      window.pageYOffset ||
-      document.documentElement.scrollTop ||
-      document.body.scrollTop;
 
-    for (let i = 0; i < sections.length; i++) {
-      const currLink = sections[i];
-      const val = currLink.getAttribute("href");
-      const refElement = document.querySelector(val);
-      const scrollTopMinus = scrollPos + 73;
-      if (
-        refElement.offsetTop <= scrollTopMinus &&
-        refElement.offsetTop + refElement.offsetHeight > scrollTopMinus
-      ) {
-        document.querySelector(".page-scroll").classList.remove("active");
-        currLink.classList.add("active");
-      } else {
-        currLink.classList.remove("active");
-      }
-    }
-  }
+    // Facts counter
+    $('[data-toggle="counter-up"]').counterUp({
+        delay: 10,
+        time: 2000
+    });
 
-  window.document.addEventListener("scroll", onScroll);
 
-  //===== close navbar-collapse when a  clicked
-  let navbarToggler = document.querySelector(".navbar-toggler");
-  const navbarCollapse = document.querySelector(".navbar-collapse");
+    // Project carousel
+    $(".project-carousel").owlCarousel({
+        autoplay: true,
+        smartSpeed: 1000,
+        margin: 25,
+        loop: true,
+        center: true,
+        dots: false,
+        nav: true,
+        navText : [
+            '<i class="bi bi-chevron-left"></i>',
+            '<i class="bi bi-chevron-right"></i>'
+        ],
+        responsive: {
+			0:{
+                items:1
+            },
+            576:{
+                items:1
+            },
+            768:{
+                items:2
+            },
+            992:{
+                items:3
+            }
+        }
+    });
 
-  document.querySelectorAll(".page-scroll").forEach((e) =>
-    e.addEventListener("click", () => {
-      navbarToggler.classList.remove("active");
-      navbarCollapse.classList.remove("show");
-    })
-  );
-  navbarToggler.addEventListener("click", function () {
-    navbarToggler.classList.toggle("active");
-  });
 
-  // WOW active
-  new WOW().init();
+    // Testimonials carousel
+    $(".testimonial-carousel").owlCarousel({
+        autoplay: true,
+        smartSpeed: 1000,
+        center: true,
+        margin: 24,
+        dots: true,
+        loop: true,
+        nav : false,
+        responsive: {
+            0:{
+                items:1
+            },
+			576:{
+                items:1
+            },
+            768:{
+                items:2
+            },
+            992:{
+                items:3
+            }
+        }
+    });
 
-  //======== tiny slider
-  tns({
-    container: ".testimonial-active",
-    slideBy: "page",
-    mode: "gallery",
-    autoplay: false,
-    mouseDrag: true,
-    gutter: 0,
-    nav: false,
-    controls: true,
-    controlsText: [
-      '<i class="lni lni-chevron-left prev"></i>',
-      '<i class="lni lni-chevron-right next"></i>',
-    ],
-    items: 1,
-  });
-})();
+    
+})(jQuery);
+
--- src/controllers/index.controller.ts
+++ src/controllers/index.controller.ts
@@ -1,11 +1,23 @@
 import { Request, Response } from "express";
+import axios from "axios";
 
 export default {
-    get(req: Request, res: Response): void {
+    async get(req: Request, res: Response): Promise<void> {
+        /* get bot data */
+        const clientStats: any = await axios.get("https://api.nevar.eu/client/stats");
+        const guildCount: number = clientStats.data?.res.server_count;
+        const userCount: number = clientStats.data?.res.user_count;
+        const voteCount: number = clientStats.data?.res.vote_count;
+        const commandCount: number = clientStats.data?.res.command_count;
+        console.log(clientStats);
         /* redirect to dashboard */
         res.render("index", {
             name: "Nevar",
-            title: "Startseite"
+            title: "Startseite",
+            voteCount: voteCount || 0,
+            guildCount: guildCount || 0,
+            userCount: userCount || 0,
+            commandCount: commandCount || 0
         });
     }
 };
