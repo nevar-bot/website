d86319c1c9ba6ab2fcf77e1d07d26773cc8700de
Jonas <p429344@gmail.com>
2023-10-15 16:47:46 +0200

    team section

--- src/controllers/index.controller.ts
+++ src/controllers/index.controller.ts
@@ -5,10 +5,12 @@ export default {
     async get(req: Request, res: Response): Promise<void> {
         /* get bot data */
         const clientStats: any = await axios.get("https://api.nevar.eu/client/stats");
+        const clientStaffs: any = await axios.get("https://api.nevar.eu/client/staffs");
         const guildCount: number = clientStats.data?.res.server_count;
         const userCount: number = clientStats.data?.res.user_count;
         const voteCount: number = clientStats.data?.res.vote_count;
         const commandCount: number = clientStats.data?.res.command_count;
+        const staffs: any = clientStaffs.data?.res.staffs;
         console.log(clientStats);
         /* redirect to dashboard */
         res.render("index", {
@@ -17,7 +19,8 @@ export default {
             voteCount: voteCount || 0,
             guildCount: guildCount || 0,
             userCount: userCount || 0,
-            commandCount: commandCount || 0
+            commandCount: commandCount || 0,
+            staffs: staffs || []
         });
     }
 };
