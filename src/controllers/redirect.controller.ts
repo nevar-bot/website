3d4f4c634de830a4f2168e36ea9dee613c202862
Jonas <p429344@gmail.com>
2023-12-10 19:25:56 +0100

    improved redirect controller

--- src/controllers/redirect.controller.ts
+++ src/controllers/redirect.controller.ts
@@ -4,11 +4,16 @@ import fs from "fs";
 
 export default {
     async get(req: Request, res: Response): Promise<void> {
+        /* Load available redirects */
         const redirects: any = toml.parse(fs.readFileSync("./redirects.toml", "utf8"));
+
+        /* Get requested redirect */
         const redirect_request: string = req.params.request.toLowerCase();
 
+        /* Check if requested redirect exists, if not redirect to home page */
         if(!redirects[redirect_request]) return res.redirect("/");
 
+        /* Redirect to requested redirect */
         res.redirect(redirects[redirect_request]);
     }
 };
