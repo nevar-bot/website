e7ec29a9c1a03a73d608d648e83cfcdfcca31260
Jonas <p429344@gmail.com>
2023-12-10 19:25:02 +0100

    improved invite controller

--- src/controllers/invite.controller.ts
+++ src/controllers/invite.controller.ts
@@ -3,8 +3,22 @@ import axios from "axios";
 
 export default {
     async get(req: Request, res: Response): Promise<void> {
-        const clientStats: any = await axios.get("https://api.nevar.eu/client/stats");
-        const invite: any = clientStats?.data.res.invite_url || "https://nevar.eu";
-        res.redirect(invite);
+        try {
+            /* Get invite url from api */
+            const { invite_url } = (await axios.get("https://api.nevar.eu/client/stats")).data.res;
+
+            /* Redirect to invite url */
+            res.status(301).redirect(invite_url);
+        }catch(error){
+            /* Render 500 error page */
+            res.status(500).render("errors/500", {
+                name: "Nevar",
+                title: "Fehler 500",
+                metaData: {
+                    description: "500 Internal Server Error",
+                    keywords: "Nevar, Discord, Bot, 500, Internal Server Error"
+                }
+            });
+        }
     }
 };
