30f8704d67539300fd43583ac4922301e5186c54
Jonas <p429344@gmail.com>
2023-12-10 19:21:49 +0100

    improved index controller

--- src/controllers/index.controller.ts
+++ src/controllers/index.controller.ts
@@ -3,30 +3,36 @@ import axios from "axios";
 
 export default {
     async get(req: Request, res: Response): Promise<void> {
-        /* get bot data */
-        const clientStats: any = await axios.get("https://api.nevar.eu/client/stats");
-        const clientStaffs: any = await axios.get("https://api.nevar.eu/client/staffs");
-        const clientCommands: any = await axios.get("https://api.nevar.eu/interactions/commands");
+        try {
+            /* Get bot data from api */
+            const stats: any = (await axios.get("https://api.nevar.eu/client/stats")).data.res;
+            const staffs: any = (await axios.get("https://api.nevar.eu/client/staffs")).data.res.staffs;
+            const commands: any = (await axios.get("https://api.nevar.eu/interactions/commands")).data.res.command_list;
 
-        const guildCount: number = clientStats.data?.res.server_count;
-        const userCount: number = clientStats.data?.res.user_count;
-        const voteCount: number = clientStats.data?.res.vote_count;
-        const commandCount: number = clientCommands.data?.res.command_list.filter((command: any): boolean => command.category !== "owner" && command.category !== "staff").length;
-        const staffs: any = clientStaffs.data?.res.staffs;
-
-        /* render index page */
-        res.render("index", {
-            name: "Nevar",
-            title: "Startseite",
-            metaData: {
-                description: "Nevar Discord-Bot",
-                keywords: "Nevar, Discord"
-            },
-            voteCount: voteCount || 0,
-            guildCount: guildCount || 0,
-            userCount: userCount || 0,
-            commandCount: commandCount || 0,
-            staffs: staffs || []
-        });
+            /* Render index page */
+            res.render("index", {
+                name: "Nevar",
+                title: "Startseite",
+                metaData: {
+                    description: "Startseite des Nevar Discord-Bots",
+                    keywords: "Nevar, Discord, Bot, Home, Start"
+                },
+                voteCount: stats.vote_count || 0,
+                guildCount: stats.server_count || 0,
+                userCount: stats.user_count || 0,
+                commandCount: commands.filter((command: any): boolean => command.category !== "owner" && command.category !== "staff").length || 0,
+                staffs: staffs || []
+            });
+        }catch(error){
+            /* Render 500 error page */
+            res.status(500).render("errors/500", {
+                name: "Nevar",
+                title: "Fehler 500",
+                metaData: {
+                    description: "500 Internal Server Error",
+                    keywords: "Nevar, Discord, Bot, 500, Internal Server Error"
+                }
+            });
+        }
     }
 };
