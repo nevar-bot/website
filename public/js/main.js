9cb75d01d4a2b1dc42f542a98a8d2226241a4183
Jonas <p429344@gmail.com>
2023-12-26 17:49:32 +0100

    Optimierungen

--- public/js/main.js
+++ public/js/main.js
@@ -113,8 +113,8 @@
    */
   on('click', '.mobile-nav-toggle', function(e) {
     select('#navbar').classList.toggle('navbar-mobile')
-    this.classList.toggle('bi-list')
-    this.classList.toggle('bi-x')
+    this.classList.toggle('fa-bars')
+    this.classList.toggle('fa-xmark')
   })
 
   /**
@@ -154,17 +154,5 @@
         scrollto(window.location.hash)
       }
     }
-  });
-
-  /**
-   * Animation on scroll
-   */
-  window.addEventListener('load', () => {
-    AOS.init({
-      duration: 1000,
-      easing: 'ease-in-out',
-      once: true,
-      mirror: false
-    })
   });
 })()
