470f7b9879d3f9ad13f21a5a1895a6a97bad7705
Jonas <jonas.pape0905@gmail.com>
2024-02-01 21:44:22 +0100

    adjust invite redirect to new api structure

--- src/controllers/invite.controller.ts
+++ src/controllers/invite.controller.ts
@@ -5,10 +5,10 @@ export default {
     async get(req: Request, res: Response): Promise<void> {
         try {
             /* Get invite url from api */
-            const { invite_url } = (await axios.get("https://api.nevar.eu/client/stats")).data.res;
+            const { invite } = (await axios.get("https://api.nevar.eu/client/stats")).data.bot;
 
             /* Redirect to invite url */
-            res.status(301).redirect(invite_url);
+            res.status(301).redirect(invite);
         }catch(error){
             /* Render 503 error page */
             res.status(503).render("errors/503", {
