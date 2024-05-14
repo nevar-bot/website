042a57efef9a1d0d9925ff5fd00af8ab35445046
Jonas <jonas.pape0905@gmail.com>
2024-05-14 20:26:17 +0200

    add support redirect

--- src/controllers/support.controller.ts
+++ src/controllers/support.controller.ts
@@ -5,11 +5,12 @@ export default {
     async get(req: Request, res: Response): Promise<void> {
         try {
             /* Get support url from api */
-            const { support_url } = (await axios.get("https://api.nevar.eu/client/stats")).data.res;
+            const { support } = (await axios.get("https://api.nevar.eu/general/stats")).data.bot;
 
             /* Redirect to support url */
-            res.status(301).redirect(support_url);
+            res.status(301).redirect(support);
         }catch(error){
+            console.log(error);
             /* Render 503 error page */
             res.status(503).render("errors/503", {
                 name: "Nevar",
